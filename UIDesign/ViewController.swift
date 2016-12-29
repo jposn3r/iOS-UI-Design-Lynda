25ba7c6b28d7db567c0bacc70744e903a697d915
Jacob Posner <jposner91@gmail.com>
2016-12-28 20:29:38 -0500

    End of sliders and progress bar video

--- UIDesign/ViewController.swift
+++ UIDesign/ViewController.swift
@@ -9,29 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progBar: UIProgressView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-    
-    @IBAction func showAlert() {
-        // Create the alert
-        let alert:UIAlertController = UIAlertController(title: "Dude!", message: "This is an alert breh..", preferredStyle: .actionSheet)
-        
-        // Define its actions
-        let action1:UIAlertAction = UIAlertAction(title: "Click me prease", style: .cancel) { (_:UIAlertAction) in print("Cancelled!")}
-        let action2:UIAlertAction = UIAlertAction(title: "Delete", style: .destructive) { (_:UIAlertAction) in print("Deleted!") }
-        
-        // Add the actions
-        alert.addAction(action1)
-        alert.addAction(action2)
-        
-        // Present the view
-        self.present(alert, animated: true) { 
-            print("present complete")
-        }
+    @IBAction func didMoveSlider(_ sender: UISlider) {
+        let percent:Float = sender.value / sender.maximumValue
+        progBar.progress = percent
     }
     
     override func didReceiveMemoryWarning() {
