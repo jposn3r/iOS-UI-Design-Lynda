2eee1febe6fc9eced868563030b219731093985c
Jacob Posner <jposner91@gmail.com>
2016-12-28 20:46:01 -0500

    Finished Segements video

--- UIDesign/ViewController.swift
+++ UIDesign/ViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var myIndicator: UIActivityIndicatorView!
-    
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
-    @IBAction func switchDidChange(_ sender: UISwitch) {
-        if !sender.isOn {
-            myIndicator.stopAnimating()
-        }
-        else {
-            myIndicator.startAnimating()
-        }
+    
+    @IBAction func segmentDidChange(_ sender: UISegmentedControl) {
+        // Yass
+        let selectedIndex = sender.selectedSegmentIndex
+        label.text = sender.titleForSegment(at: selectedIndex)
     }
     
     override func didReceiveMemoryWarning() {
