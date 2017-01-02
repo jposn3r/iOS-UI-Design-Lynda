e9161af3b058a980a7f369de90242b0e78cf247a
Jacob Posner <jposner91@gmail.com>
2017-01-02 16:13:55 -0500

    Screen rotation with varying box layout

--- UIDesign/ViewController.swift
+++ UIDesign/ViewController.swift
@@ -8,35 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    let items:[[String]] = [
-        ["A1", "A2", "A3"],
-        ["B1", "B2"],
-        ["C1", "C2"]
-    ]
-    @IBOutlet weak var label: UILabel!
+class ViewController: UIViewController, UIPickerViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return items.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return items[component].count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return items[component][row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        label.text = items[component][row]
-    }
-    
     
 
     override func didReceiveMemoryWarning() {
